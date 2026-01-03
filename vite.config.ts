395a301a216f73bfb88d9fc3523abc5391cd7337
Anthony Corbin <dragon1672@gmail.com>
2026-01-03 14:22:17 -0500

    updating deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: "/gemini-dual-letter-genny/",
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
